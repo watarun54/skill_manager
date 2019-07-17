606ceda9495dc29281b8e9597370ba41f098699a
watarun <watarun54@gmail.com>
2019-07-17 22:41:42 +0700

    change controller name 'top' to 'cards'

--- test/controllers/cards_controller_test.rb
+++ test/controllers/cards_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class TopControllerTest < ActionDispatch::IntegrationTest
+class CardsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
