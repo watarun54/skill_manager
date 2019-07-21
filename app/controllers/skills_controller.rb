1bae37a020794ee71be9f04793aabf618c486a35
watarun <watarun54@gmail.com>
2019-07-21 22:28:16 +0700

    dev-1: create create action as ajax

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,5 +1,6 @@
 class SkillsController < ApplicationController
   def index
+    @skill ||= Skill.new
     @skills = Skill.all.order(id: "DESC")
   end
 
@@ -12,7 +13,19 @@ class SkillsController < ApplicationController
   end
 
   def create
+		@skill = Skill.new(skill_params)
 
+		respond_to do |format|
+			if @skill.save
+        format.html { render :index }
+        format.json { render :index, status: :created, location: @skill }
+        format.js { @status = "success"}
+      else
+        format.html { render :index }
+        format.json { render json: @skill.errors, status: :unprocessable_entity }
+        format.js { @status = "fail" }
+      end
+    end
   end
 
   def edit
@@ -25,5 +38,14 @@ class SkillsController < ApplicationController
 
   def destroy
 
+  end
+
+  private
+  def skill_params
+    params.require(:skill).permit(:name)
+  end
+
+  def set_skill
+    @skill = Skill.find(params[:id])
   end
 end
