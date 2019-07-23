bd9adf4715f03aa7901d2f36f0e56cd59535f47f
watarun <watarun54@gmail.com>
2019-07-23 21:04:36 +0700

    dev-3: add 'turbolinks:load' to enable jquery function

--- app/assets/javascripts/dashboards.js
+++ app/assets/javascripts/dashboards.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).on('turbolinks:load', function() {
   $('.dropdown-menu .nav-link').click(function(){
     var visibleItem = $('#period-button');
     visibleItem.text($(this).text());
