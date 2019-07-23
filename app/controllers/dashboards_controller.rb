3fdf690b33fb4a940cb4a098b1633c875998d7ce
watarun <watarun54@gmail.com>
2019-07-23 18:26:05 +0700

    dev-2: add scope in_a_week/month/year in card model

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,6 +1,10 @@
 class DashboardsController < ApplicationController
   def index
-    @skill_score_chart = Skill.all.map { |skill| [skill.name, skill.cards.sum(:score)] }
+    @skill_score_chart_per_week = Skill.all.map { |skill| [skill.name, skill.cards.in_a_week.sum(:score)] }
+    @skill_score_chart_per_month = Skill.all.map { |skill| [skill.name, skill.cards.in_a_month.sum(:score)] }
+    @skill_score_chart_per_year = Skill.all.map { |skill| [skill.name, skill.cards.in_a_year.sum(:score)] }
+
+
     @skill_card_chart = Skill.all.map { |skill| [skill.name, skill.cards.count] }
   end
 end
