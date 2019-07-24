7d6bbdf9c18adec08b9189338314e51c8c3d3800
watarun <watarun54@gmail.com>
2019-07-24 19:43:50 +0700

    dev-3: create skill_score/card_chart in all period

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -11,10 +11,12 @@ class DashboardsController < ApplicationController
     @skill_score_chart_per_week = Skill.all.map { |skill| [skill.name, skill.cards.in_a_week.sum(:score)] }
     @skill_score_chart_per_month = Skill.all.map { |skill| [skill.name, skill.cards.in_a_month.sum(:score)] }
     @skill_score_chart_per_year = Skill.all.map { |skill| [skill.name, skill.cards.in_a_year.sum(:score)] }
+    @skill_score_chart = Skill.all.map { |skill| [skill.name, skill.cards.sum(:score)] }
 
     @skill_card_chart_per_week = Skill.all.map { |skill| [skill.name, skill.cards.in_a_week.count] }
     @skill_card_chart_per_month = Skill.all.map { |skill| [skill.name, skill.cards.in_a_month.count] }
     @skill_card_chart_per_year = Skill.all.map { |skill| [skill.name, skill.cards.in_a_year.count] }
+    @skill_card_chart = Skill.all.map { |skill| [skill.name, skill.cards.count] }
   end
 
   private
