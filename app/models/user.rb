4679e0a78a9858061fc1e2ed610067acb5ac94ae
watarun <watarun54@gmail.com>
2019-07-27 20:24:09 +0700

    dev-7: add maxlength and type to form field (user/session model)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
   has_secure_password validations: true
 
-  validates :name, presence: true, length: { maximum: 20 }
+  validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false },
+                    length: { maximum: 191 }
 
   def self.new_remember_token
     SecureRandom.urlsafe_base64
