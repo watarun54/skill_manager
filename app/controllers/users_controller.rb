e66537c18ac9f096f94a24aa6f558d34608c9c68
watarun <watarun54@gmail.com>
2019-07-28 15:23:46 +0700

    dev-9: Create user edit/delete functions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   skip_before_action :login_required, only: [:new, :create]
   before_action :redirect_login_user, only: [:new, :create]
+  before_action :set_user, only: [:edit, :update, :destroy]
 
   def new
     @user = User.new
@@ -19,7 +20,32 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "Account updated successfully!"
+      redirect_to edit_user_path(@user.id)
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      flash[:notice] = "Account deleted successfully!"
+      redirect_to login_path
+    else
+      render "edit"
+    end
+  end
+
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
