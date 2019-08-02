42478e5b15f10ee503bca757f0f9e38f6b6748f5
watarun <watarun54@gmail.com>
2019-08-02 19:42:12 +0700

    dev-13: update paper model

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,2 +1,11 @@
 class Paper < ApplicationRecord
+  belongs_to :user
+  belongs_to :general_skill
+
+  validates :title, length: { maximum: 100 }
+  validates :content, length: { maximum: 500 }
+  validates :url, length: { maximum: 300 }
+  validates :user_id, presence: true
+
+  scope :of_current_user, ->(current_user) { current_user.papers }
 end
