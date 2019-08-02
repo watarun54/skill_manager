a3bc3da82b48cb4521c93102be993a9c7ebdffd5
watarun <watarun54@gmail.com>
2019-08-02 19:31:16 +0700

    dev-13: generate Paper model

---
+++ db/migrate/20190802121354_create_papers.rb
@@ -0,0 +1,14 @@
+class CreatePapers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :papers do |t|
+      t.string :title
+      t.text :content
+      t.integer :status
+      t.text :url
+      t.references :user, foreign_key: true
+      t.references :general_skill
+
+      t.timestamps
+    end
+  end
+end
