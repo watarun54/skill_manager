e024732f6897570d49701192001db14d26df8b2b
watarun <watarun54@gmail.com>
2019-08-06 22:21:52 +0700

    dev-18: refactor truncate_paper_title

--- app/helpers/papers_helper.rb
+++ app/helpers/papers_helper.rb
@@ -1,11 +1,8 @@
 module PapersHelper
   def truncate_paper_title(title, num)
     title = title.try(:truncate, 50)
-    if title.nil?
-      ""
-    else
-      arr = title.scan(/.{1,#{num}}/)
-      simple_format(arr.join('<br>'))
-    end
+    return "" if title.nil?
+    arr = title.scan(/.{1,#{num}}/)
+    simple_format(arr.join('<br>'))
   end
 end
