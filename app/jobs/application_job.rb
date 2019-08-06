f91af2dfa449ffb75835cc69418b9cd8474eb82a
watarun <watarun54@gmail.com>
2019-08-06 19:30:37 +0700

    dev-18: add retry logic of job

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,2 +1,34 @@
 class ApplicationJob < ActiveJob::Base
+  DEFAULT_RETRY_LIMIT = 0
+
+  attr_reader :attempt_number
+
+  class << self
+    def retry_limit(retry_limit)
+      @retry_limit = retry_limit
+    end
+
+    def load_retry_limit
+      @retry_limit || DEFAULT_RETRY_LIMIT
+    end
+  end
+
+  def serialize
+    super.merge("attempt_number" => (@attempt_number || 0) + 1)
+  end
+
+  def deserialize(job_data)
+    super
+    @attempt_number = job_data["attempt_number"]
+  end
+
+  private
+
+  def retry_limit
+    self.class.load_retry_limit
+  end
+
+  def retry_limit_exceeded?
+    @attempt_number > retry_limit
+  end
 end
--- app/jobs/update_papers_job.rb
+++ app/jobs/update_papers_job.rb
@@ -1,5 +1,11 @@
 class UpdatePapersJob < ApplicationJob
   queue_as :default
+  retry_limit 0
+
+  rescue_from(StandardError) do |exception|
+    raise exception if retry_limit_exceeded?
+    retry_job(wait: attempt_number**2)
+  end
 
   def perform(user_id, text, uri)
     logger = Rails.application.config.update_papers_logger
