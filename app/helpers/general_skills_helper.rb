9c7fe288598a429a5f7cb8aa4972d982cdfed49b
watarun <watarun54@gmail.com>
2019-08-12 19:01:08 +0700

    dev-22: add link to general_skill text and add column to general_skills table

--- app/helpers/general_skills_helper.rb
+++ app/helpers/general_skills_helper.rb
@@ -1,2 +1,8 @@
 module GeneralSkillsHelper
+  def total_score_of_all_cards_in(gs)
+    gs.skills.map {|skill| skill.cards.sum(:score) }.sum
+  end
+  def total_num_of_all_cards_in(gs)
+    gs.skills.map {|skill| skill.cards.count }.sum
+  end
 end
