6fef306adc2bc2cc6d7b8c9da63267bd2953fe8e
watarun <watarun54@gmail.com>
2019-08-25 20:59:33 +0700

    dev-26: fix bug for nav bar to open when clicking

--- app/assets/javascripts/layouts.js
+++ app/assets/javascripts/layouts.js
@@ -1,4 +1,11 @@
 $(document).on('turbolinks:load', function() {
+  // クリック時、メニューを閉じる
+  $('.navbar-nav a').on('click', function(){
+    if($(this).attr('class').indexOf('collapse-dashboard') == -1){
+      $('.navbar-collapse').collapse('hide');
+    }
+  });
+  // 「Dashboard」のクリック時、G.S.のリストを表示する
   $('.collapse-dashboard').click(function(){
     $('.dashboard').toggleClass("open");
   });
