11302afbc329206835bd542cea30fe73e12da299
watarun <watarun54@gmail.com>
2020-02-03 00:31:54 +0900

    dev-31: remove unnecessary blank

--- lib/aws/rekognition_adapter.rb
+++ lib/aws/rekognition_adapter.rb
@@ -18,23 +18,23 @@ class Aws::RekognitionAdapter
   # コレクションを削除
   def delete_collection(collection_id)
     @client.delete_collection({
-      collection_id: collection_id, 
+      collection_id: collection_id,
     })
   end
 
   # コレクションに顔画像を追加
   def add_face_to_collection(user_id, c_id, s3_bucket, filename)
     res = @client.index_faces({
-      collection_id: c_id, 
+      collection_id: c_id,
       detection_attributes: [
-      ], 
-      external_image_id: user_id.to_s, 
+      ],
+      external_image_id: user_id.to_s,
       image: {
         s3_object: {
-          bucket: s3_bucket, 
-          name: filename, 
-        }, 
-      }, 
+          bucket: s3_bucket,
+          name: filename,
+        },
+      },
     })
     face_id = res.face_records[0].face.face_id
     User.find(user_id).create_face_image(filename: filename, face_id: face_id)
@@ -52,22 +52,22 @@ class Aws::RekognitionAdapter
   # コレクション内の顔画像一覧
   def get_faces(c_id)
     @client.list_faces({
-      collection_id: c_id, 
+      collection_id: c_id,
     })
   end
 
   # コレクションの中から一致する顔画像を検索
   def search_faces_by_image(c_id, s3_bucket, filename)
     @client.search_faces_by_image({
-      collection_id: c_id, 
-      face_match_threshold: 99, 
+      collection_id: c_id,
+      face_match_threshold: 99,
       image: {
         s3_object: {
-          bucket: s3_bucket, 
-          name: filename, 
-        }, 
-      }, 
-      max_faces: 1, 
+          bucket: s3_bucket,
+          name: filename,
+        },
+      },
+      max_faces: 1,
     })
   end
 end
